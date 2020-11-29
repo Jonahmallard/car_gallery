ba19b52afce60067a45b3ae2d9f947ab1ef90537
jonah <benjaminjmallard@gmail.com>
2020-11-28 22:38:25 -0600

    upvote/downvote functionality

--- car_gallery_api/db/seeds.rb
+++ car_gallery_api/db/seeds.rb
@@ -6,6 +6,7 @@ cars =  [
       "model": "Scion FR-S",
       "image": "https://file.kelleybluebookimages.com/kbb/base/evox/StJ/10531/2016-Scion-FR-S-front-angle6_10531_116_640x480.jpg",
       "likes": 5,
+      "dislikes": 1,
       "category_id": 1
     },
     {
@@ -15,6 +16,7 @@ cars =  [
       "model": "Skyline",
       "image": "https://bestcarmag.com/sites/default/files/91477331996_nissan_skyline-pic-7446485517683503954.jpeg",
       "likes": 8,
+      "dislikes": 1,
       "category_id": 1
     },
     {
@@ -24,6 +26,7 @@ cars =  [
       "model": "Nova",
       "image": "https://cdn.classicdigest.com/live/carimg/20501_20600/20504_f9d6824b8d407667.jpg",
       "likes": 3,
+      "dislikes": 1,
       "category_id": 1
     },
     {
@@ -33,6 +36,7 @@ cars =  [
       "model": "Cooper",
       "image": "https://bringatrailer.com/wp-content/uploads/2019/10/1967_austin_mini_cooper_s_1571942346c519f6b34c9898DSC_0437-e1571966832781.jpg?fit=940%2C627",
       "likes": 4,
+      "dislikes": 1,
       "category_id": 1
     },
     {
@@ -42,6 +46,7 @@ cars =  [
       "model": "F-150 SVT Raptor",
       "image": "https://s1.cdn.autoevolution.com/images/models/FORD_F-150-SVT-Raptor-2017_main.jpg",
       "likes": 1,
+      "dislikes": 1,
       "category_id": 2
     },
     {
@@ -51,6 +56,7 @@ cars =  [
       "model": "Ranger",
       "image": "https://s1.cdn.autoevolution.com/images/models/FORD_F-150-SVT-Raptor-2017_main.jpg",
       "likes": 2,
+      "dislikes": 1,
       "category_id": 2
     },
     {
@@ -60,6 +66,7 @@ cars =  [
       "model": "Grom",
       "image": "https://s1.cdn.autoevolution.com/images/models/FORD_F-150-SVT-Raptor-2017_main.jpg",
       "likes": 3,
+      "dislikes": 1,
       "category_id": 3
     },
     {
@@ -69,6 +76,7 @@ cars =  [
       "model": "Raptor",
       "image": "https://s1.cdn.autoevolution.com/images/models/FORD_F-150-SVT-Raptor-2017_main.jpg",
       "likes": 1,
+      "dislikes": 1,
       "category_id": 3
     }
   ]
@@ -80,5 +88,5 @@ Category.create(name: "Trucks")
 Category.create(name: "Recreational Vehicles")
 
 cars.each do |car|
-    Car.create(year: car[:year], make: car[:make], model: car[:model], image: car[:image], likes: car[:likes], category_id: car[:category_id])
+    Car.create(year: car[:year], make: car[:make], model: car[:model], image: car[:image], likes: car[:likes], dislikes: car[:dislikes], category_id: car[:category_id])
 end
